27ccef5b7fc8a82dd65e1f8463847333c0b4cf65
AevumDecessus <aevum@decess.us>
2024-07-17 12:19:17 -0400

    Updated motd script to also generate butane

--- motd.sh
+++ motd.sh
@@ -5,14 +5,34 @@ echo $SOURCE_DIR
 source ${SOURCE_DIR}/arrays.sh
 source ${SOURCE_DIR}/colors.sh
 
+make_dirs() {
+  mkdir -p ${SOURCE_DIR}/motd
+  mkdir -p ${SOURCE_DIR}/butane/motd
+  mkdir -p ${SOURCE_DIR}/ignition/motd
+}
 do_hosts() {
-  for host in "${hosts[@]}"
+  for HOST in "${hosts[@]}"
   do
-    OUT=${SOURCE_DIR}/motd/${host}
+    OUT=${SOURCE_DIR}/motd/${HOST}
     #echo ${RED} > ${OUT}
-    figlet ${host} > ${OUT}
+    figlet ${HOST} > ${OUT}
     #echo ${NONE} >> ${OUT}
+    output_butane
   done
 }
 
+output_butane() {
+  cat << EOF > ${SOURCE_DIR}/butane/motd/${HOST}.bu
+variant: fcos
+version: 1.5.0
+storage:
+  files:
+    - path: /etc/motd.d/00-figlet.conf
+      contents:
+        source: https://raw.githubusercontent.com/AevumDecessus/ignition-configs/main/motd/$HOST
+      mode: 0644
+EOF
+}
+
+make_dirs
 do_hosts
