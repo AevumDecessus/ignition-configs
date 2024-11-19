2cde8d9cfa6df7b3b9d4ea6abcd6f25b91694eae
AevumDecessus <aevum@decess.us>
2024-11-19 09:51:45 -0500

    Moved fixer mount to mounts folder
    Updated said mount on all hosts to nfs3 from nfs4

--- arrays.sh
+++ arrays.sh
@@ -1,4 +1,4 @@
 declare -a docker=("apps" "books" "db" "downloads" "misc" "network" "plex" "secure" "simple" "web")
 declare -a merge=("butane/common/merge.bu" "butane/common/merge_no_packages.bu" "butane/ucore/merge-ucore.bu" "butane/nvidia/merge-nvidia.bu")
 declare -a hosts=("apps" "books" "db" "downloads" "lead1" "lead2" "misc" "network" "network2" "plex" "secure" "web")
-declare -a mounts=("3d_models" "anime" "anime_movies" "audiobooks" "audiobooks_unsorted" "calibre" "camera" "cartoons" "comics" "digikam" "documentary_films" "documentary_series" "downloads" "kids_tv" "live_media" "movies_4k" "movies" "music" "photos" "plex_meta" "sabnzbd" "tv_4k" "tv")
+declare -a mounts=("3d_models" "anime" "anime_movies" "audiobooks" "audiobooks_unsorted" "calibre" "camera" "cartoons" "comics" "digikam" "documentary_films" "documentary_series" "downloads" "fixer_mount" "kids_tv" "live_media" "movies_4k" "movies" "music" "photos" "plex_meta" "sabnzbd" "tv_4k" "tv")
