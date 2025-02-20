4adff54f6ed8144ec1e24fb064c4b16cde9a6c03
AevumDecessus <aevum@decess.us>
2025-02-20 16:28:29 -0500

    Added lead3-lead5 hosts
    removed docker mounts for lead hosts for failover

--- arrays.sh
+++ arrays.sh
@@ -1,4 +1,4 @@
 declare -a docker=("apps" "books" "db" "downloads" "misc" "network" "plex" "secure" "simple" "social" "web")
 declare -a merge=("butane/common/merge.bu" "butane/common/merge_no_packages.bu" "butane/common/merge_remote.bu" "butane/ucore/merge-ucore.bu" "butane/ucore/merge-ucore-remote.bu" "butane/nvidia/merge-nvidia.bu" "butane/nvidia/merge-nvidia-zfs.bu")
-declare -a hosts=("apps" "books" "db" "downloads" "lead1" "lead2" "misc" "network" "network2" "plex" "secure" "social" "web")
+declare -a hosts=("apps" "books" "db" "downloads" "lead1" "lead2" "lead3" "lead4" "lead5" "misc" "network" "network2" "plex" "secure" "social" "web")
 declare -a mounts=("3d_models" "anime" "anime_movies" "audiobooks" "audiobooks_unsorted" "calibre" "camera" "cartoons" "comics" "digikam" "documentary_films" "documentary_series" "downloads" "fixer_mount" "kids_tv" "live_media" "movies_4k" "movies" "music" "photos" "piwigo" "plex_meta" "sabnzbd" "tv_4k" "tv")
